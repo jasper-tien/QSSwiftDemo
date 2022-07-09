808e99206575ddeff8eff3740ff6ecfda9a7db3b
slowtian <maotaotian@yeah.net>
2022-07-09 10:46:16 +0800

    注释

--- QSSwiftDemo/QSSwiftDemo/ViewController.swift
+++ QSSwiftDemo/QSSwiftDemo/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    // 注释
     func funcString(_ str: String) {
         print("desc:\(str)")
     }
