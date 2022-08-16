a79792169ff2c7d94c56dc74c124a90724b51841
tianmaotao <tianmaotao@bilibili.com>
2022-08-16 20:42:46 +0800

    QSTabBarItem逻辑填充

--- QSSwiftDemo/QSSwiftDemo/TabController/QSTabBarItem.swift
+++ QSSwiftDemo/QSSwiftDemo/TabController/QSTabBarItem.swift
@@ -10,30 +10,189 @@ import UIKit
 
 public protocol QSTabBarItemProtocol {
     var itemIndex: UInt { get }
-    var itemWidth: Float { get }
+    var itemWidth: CGFloat { get }
     func config(with index: UInt)
-    func config(with customWidth: Float)
+    func config(with customWidth: CGFloat)
     func config(with actionBlock: @escaping (UInt, UInt) -> Void)
     func update(with isHighlight: Bool)
     func setupMaxTextLengthLimit(_ limit: UInt)
 }
 
-public class QSTabBarItem: UIView,  QSTabBarItemProtocol {
+class QSTabBarItemElement {
+    var font: UIFont?
+    var titleColor: UIColor?
+    var titleNightColor: UIColor?
+    var titleSelectColor: UIColor?
+    var titleSelectNightColor: UIColor?
+}
+
+public class QSTabBarItem: UIView {
     private var actionBlock: ((UInt, UInt) -> Void)?
     private var isHighlight: Bool = false
-    private var customWidth: Float = 0
+    private var customWidth: CGFloat = 0
+    private var hasWidth = false
+    private var autoWidth: CGFloat = 0
     private var index: UInt = 0
     private var limit: UInt = 0
+    private lazy var titleElement = QSTabBarItemElement()
+    private lazy var subtitleElement = QSTabBarItemElement()
+    private var titleAttriStr: NSMutableAttributedString?
+    private var subtitleAttriStr: NSMutableAttributedString?
+    private lazy var contentLabel = UILabel()
+    private var disableTheme = false
+    private var isDarkTheme: Bool {
+        return false
+    }
     
-    //MARK: QSTabBarItemProtocol
-    public var itemIndex: UInt  = 0
-    public var itemWidth: Float = 0
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+    public func config(title: String?, subtitle: String?) {
+        hasWidth = false
+        var hasTitle = false
+        if title != nil {
+            hasTitle = true
+            titleAttriStr = NSMutableAttributedString(string: title!)
+        } else {
+            titleAttriStr = nil;
+        }
+        if subtitle != nil {
+            let blankStr = hasTitle ? " " : ""
+            subtitleAttriStr = NSMutableAttributedString(string: "\(blankStr)\(subtitle!)")
+        } else {
+            subtitleAttriStr = nil
+        }
+        self.setNeedsLayout()
+    }
+    
+    public func config(titleFont: UIFont?, subtitleFont: UIFont?) {
+        hasWidth = false
+        titleElement.font = titleFont ?? UIFont.systemFont(ofSize: 13)
+        subtitleElement.font = subtitleFont ?? UIFont.systemFont(ofSize: 10)
+        self.setNeedsLayout()
+    }
+    
+    public func config(titleColor: UIColor?, isNight: Bool) {
+        if isNight {
+            titleElement.titleNightColor = titleColor
+        } else {
+            titleElement.titleColor = titleColor
+        }
+    }
+    
+    public func config(subtitleColor: UIColor?, isNight:Bool) {
+        if isNight {
+            subtitleElement.titleNightColor = subtitleColor
+        } else {
+            subtitleElement.titleColor = subtitleColor
+        }
+    }
+    
+    public func configHightlight(titleColor: UIColor?, isNight: Bool) {
+        if isNight {
+            titleElement.titleSelectNightColor = titleColor
+        } else {
+            titleElement.titleSelectColor = titleColor
+        }
+    }
+    
+    public func configHightlight(subtitleColor: UIColor?, isNight: Bool) {
+        if isNight {
+            subtitleElement.titleSelectNightColor = subtitleColor
+        } else {
+            subtitleElement.titleSelectColor = subtitleColor
+        }
+    }
+    
+    private func setupSubviews() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(clickAction(_:)))
+        self.addGestureRecognizer(tapGesture)
+        
+        let doubleTapGesture = UITapGestureRecognizer(target: self, action: #selector(clickAction(_:)))
+        doubleTapGesture.numberOfTapsRequired = 2
+        self.addGestureRecognizer(doubleTapGesture)
+        
+        contentLabel.textAlignment = .center
+        self .addSubview(contentLabel)
+    }
+    
+    private func currentTextColor(isTitle: Bool) -> UIColor {
+        let textElement = isTitle ? titleElement : subtitleElement;
+        let isValidDarkTheme = isDarkTheme && !disableTheme;
+        if (isHighlight) {
+            if (isValidDarkTheme) {
+                // 高亮夜间模式
+                return textElement.titleSelectNightColor ?? UIColor.systemPink
+            } else {
+                // 高亮白色主题
+                return textElement.titleSelectColor ?? UIColor.systemPink
+            }
+        } else {
+            if (isValidDarkTheme) {
+                // 未选中夜间模式
+                return textElement.titleNightColor ?? UIColor.black
+            } else {
+                // 未选中白色主题
+                return textElement.titleColor ?? UIColor.black
+            }
+        }
+    }
+    
+    private func contentTitleAttributedString() -> NSMutableAttributedString {
+        let attributedTitle = NSMutableAttributedString()
+        if titleAttriStr != nil {
+            let attributes = [
+                NSAttributedString.Key.font: titleElement.font ?? UIFont.systemFont(ofSize: 13),
+                NSAttributedString.Key.strokeColor: currentTextColor(isTitle: true)
+            ]
+            titleAttriStr!.addAttributes(attributes, range: NSRange(location: 0, length: titleAttriStr!.length))
+            attributedTitle.append(titleAttriStr!)
+        }
+        if subtitleAttriStr != nil {
+            let attributes = [
+                NSAttributedString.Key.font: subtitleElement.font ?? UIFont.systemFont(ofSize: 10),
+                NSAttributedString.Key.strokeColor: currentTextColor(isTitle: false)
+            ]
+            subtitleAttriStr!.addAttributes(attributes, range: NSRange(location: 0, length: subtitleAttriStr!.length))
+            attributedTitle.append(subtitleAttriStr!)
+        }
+        return attributedTitle
+    }
+    
+    private func contentTitleWidth() -> CGFloat {
+        if customWidth > 0 {
+            return customWidth
+        }
+        if hasWidth {
+            return autoWidth
+        }
+        hasWidth = true
+        let attributedText = contentTitleAttributedString()
+        autoWidth = attributedText.boundingRect(with: CGSize(width: 0, height: 20.0), options: NSStringDrawingOptions.usesLineFragmentOrigin, context: nil).width
+        return autoWidth
+    }
+    
+    // MARK: evnents
+    @objc
+    private func clickAction(_ tap: UITapGestureRecognizer) {
+        self.actionBlock?(itemIndex, (tap.numberOfTapsRequired == 2) ? 2 : 1)
+    }
+}
+
+extension QSTabBarItem : QSTabBarItemProtocol {
+    public var itemIndex: UInt {
+        index
+    }
+    public var itemWidth: CGFloat {
+        contentTitleWidth()
+    }
     
     public func config(with index: UInt) {
         self.index = index
     }
     
-    public func config(with customWidth: Float) {
+    public func config(with customWidth: CGFloat) {
         self.customWidth = customWidth
     }
     
